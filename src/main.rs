7d171e39cf21099e9d20de86ee2c57f8a71323af
MMinusOne <michael.business.lb@proton.me>
2025-12-13 23:14:50 +0200

    serial port

--- src/main.rs
+++ src/main.rs
@@ -5,30 +5,59 @@
 #![test_runner(crate::test_runner)]
 #[cfg(test)]
 pub fn test_runner(tests: &[&dyn Fn()]) {
-    println!("Running {} tests", tests.len());
+    serial_println!("Running {} tests", tests.len());
     for test in tests {
         test();
     }
+
+    exit_qemu(QemuExitCode::Success);
 }
 
 use crate::vga_buffer::WRITER;
 use core::panic::PanicInfo;
 
+mod serial;
 mod vga_buffer;
 
 #[unsafe(no_mangle)]
 pub extern "C" fn _start() -> ! {
-    println!("Hello World{}", "!");
+    serial_println!("Hello World{}", "!");
 
     #[cfg(test)]
     test_main();
 
     loop {}
 }
+#[derive(Debug, Clone, Copy, PartialEq, Eq)]
+#[repr(u32)]
+pub enum QemuExitCode {
+    Success = 0x10,
+    Failed = 0x11,
+}
 
+pub fn exit_qemu(exit_code: QemuExitCode) {
+    use x86_64::instructions::port::Port;
+
+    unsafe {
+        const IO_BASE: u16 = 0xf4;
+        let mut port = Port::new(IO_BASE);
+        port.write(exit_code as u32);
+    }
+}
+
+#[cfg(not(test))]
 #[panic_handler]
 fn panic(info: &PanicInfo) -> ! {
-    println!("{:#?}", info);
+    serial_println!("{:#?}", info);
+    loop {}
+}
+
+#[cfg(test)]
+#[panic_handler]
+fn panic(info: &PanicInfo) -> ! {
+    serial_println!("[failed]\n");
+    serial_println!("Error: {}\n", info);
+    exit_qemu(QemuExitCode::Failed);
     loop {}
 }
 
@@ -36,5 +65,5 @@ fn panic(info: &PanicInfo) -> ! {
 fn trivial_assertation() {
     print!("trivial assertion... ");
     assert_eq!(1, 1);
-    println!("[ok]");
+    serial_println!("[ok]");
 }
