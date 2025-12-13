9399aea396fcd2ee217d73ba527200e1af6b31e8
MMinusOne <michael.business.lb@proton.me>
2025-12-13 21:51:29 +0200

    latest

--- src/main.rs
+++ src/main.rs
@@ -1,17 +1,40 @@
 #![no_std]
 #![no_main]
+#![reexport_test_harness_main = "test_main"]
+#![feature(custom_test_frameworks)]
+#![test_runner(crate::test_runner)]
+#[cfg(test)]
+pub fn test_runner(tests: &[&dyn Fn()]) {
+    println!("Running {} tests", tests.len());
+    for test in tests {
+        test();
+    }
+}
 
+use crate::vga_buffer::WRITER;
 use core::panic::PanicInfo;
 
 mod vga_buffer;
 
 #[unsafe(no_mangle)]
 pub extern "C" fn _start() -> ! {
-    println!("dwdsadasd");
+    println!("Hello World{}", "!");
+
+    #[cfg(test)]
+    test_main();
+
     loop {}
 }
 
 #[panic_handler]
-fn panic(_info: &PanicInfo) -> ! {
+fn panic(info: &PanicInfo) -> ! {
+    println!("{:#?}", info);
     loop {}
+}
+
+#[test_case]
+fn trivial_assertation() {
+    print!("trivial assertion... ");
+    assert_eq!(1, 1);
+    println!("[ok]");
 }
